2ed6b58d0642247ba6e5775b146ae333b4e5eb06
Thomas Witt <tom.89@gmx.at>
2020-09-07 11:37:46 +0200

    two arrays save the day.

---
+++ src/Maximum2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Maximum2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please type in your numbers: end with q");
+        int[] numbers = new int[0];
+        int[] moreNumbers;
+        int highestNumber = 0;  // The program will not run, if there is a possibility a variable might not be initialized.
+        // This can only happen here, if the user types no number or just one number. In this case the user is stupid and i will (at this moment)
+        // not create an explaining message and a loop to continue the program.
+        while (!scanner.hasNext("q")) {
+            moreNumbers = new int[numbers.length + 1];
+            for (int i = 0; i < numbers.length; i++) {
+                moreNumbers[i] = numbers[i];
+            }
+            moreNumbers[moreNumbers.length - 1] = scanner.nextInt();
+            if (moreNumbers.length == 1) {
+                highestNumber = moreNumbers[0];
+            } else if (moreNumbers[moreNumbers.length - 1] > highestNumber && moreNumbers[moreNumbers.length - 1] > moreNumbers[moreNumbers.length - 2]) {
+                highestNumber = moreNumbers[moreNumbers.length - 1];
+            }
+            numbers = moreNumbers;
+        }
+        System.out.println(highestNumber);
+    }
+}
